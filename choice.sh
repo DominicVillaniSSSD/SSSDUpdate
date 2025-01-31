a4dbc3a49836e5d4ab181d0a6857ecb95e92cebf
Dominic Villani <dvillani@sssd.k12.ca.us>
2025-01-30 16:54:30 -0800

    Update choice.sh

--- choice.sh
+++ choice.sh
@@ -1,6 +1,60 @@
 #!/bin/bash
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+BROWN='\033[0;33;2m' # Dim Yellow to approximate Brown
+ORANGE='\033[0;31;1m' # Adjusted to be more orange
+NC='\033[0m' # No Color
+
+stop_smart_software() {
+    echo -e "\033[33mStopping SMART Technologies software...\033[0m"
+    
+    # Kill all SMART Technologies related processes by executable name
+    sudo pkill -f "/Applications/SMART Technologies/SMART Settings.app/Contents/bin/sbsdk-server/SBWDKService" || true
+    sudo pkill -f "/Applications/SMART Technologies/SMART Ink.app/Contents/MacOS/SMART Ink" || true
+    sudo pkill -f "/Applications/SMART Technologies/SMART Settings.app/Contents/bin/SystemNotifications.app/Contents/MacOS/SystemNotifications" || true
+    sudo pkill -f "/Applications/SMART Technologies/SMARTBoardService.app/Contents/MacOS/SMARTBoardService" || true
+    sudo pkill -f "/Applications/SMART Technologies/SMARTSystemMenu.app/Contents/MacOS/SMARTSystemMenu" || true
+    sudo pkill -f "crashpad_handler2" || true
+
+    # Unload any launch daemons or agents
+    if [ -e "/Library/LaunchDaemons/com.smarttech.*" ]; then
+        sudo launchctl unload /Library/LaunchDaemons/com.smarttech.*
+    fi
+    if [ -e "/Library/LaunchAgents/com.smarttech.*" ]; then
+        sudo launchctl unload /Library/LaunchAgents/com.smarttech.*
+    fi
+
+    echo -e "\033[32mSMART Technologies software services stopped.\033[0m"
+    sleep 0.5
+}
+
+start_smart_software(){
+    echo -e "\033[33mStarting SMART Technologies software...\033[0m"
+
+    echo -e "\033[33mStarting SMART Board Service...\033[0m"
+    open "/Applications/SMART Technologies/SMARTBoardService.app"
+    echo -e "\033[33mStarting SMART System Menu...\033[0m"
+    open "/Applications/SMART Technologies/SMARTSystemMenu.app"
+    echo -e "\033[33mStarting SMART Ink...\033[0m"
+    open "/Applications/SMART Technologies/SMART Ink.app"
+    echo -e "\033[33mStarting SMART Settings...\033[0m"
+    open "/Applications/SMART Technologies/SMART Settings.app"
+    sleep 0.5
+}
+
+restart_smart_software(){
+    stop_smart_software
+    start_smart_software
+}
+
 cleanupSmartSoftware() {
+    stop_smart_software
     echo "Cleaning up SMART Technologies software..."
 
     # Remove main application and library support files
@@ -36,6 +90,7 @@ cleanupSmartSoftware() {
 }
 
 fullcleanupSmartSoftware() {
+    stop_smart_software
     echo "Cleaning up SMART Technologies software..."
 
     # Remove main application and library support files
@@ -70,14 +125,26 @@ fullcleanupSmartSoftware() {
     echo "SMART Technologies software full cleanup completed."
 }
 
+add_everyone_to_printer_group(){
+    #add everyone to lpadmin group to allow everyone to add a printer
+    echo -e "${GREEN}Adding everyone to lpadmin group to allow everyone to add a printer...${NC}"
+    sudo /usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group lpadmin
+    sleep 0.5
+}
+
 install_applications_teacher(){
+    add_everyone_to_printer_group
     install_application_from_url "$app_cleaner_url"
     install_application_from_url "$zoom_url"
+    fullcleanupSmartSoftware
     install_application_from_url "$smart_notebook_url"
+    start_smart_software
     install_application_from_url "$google_chrome_url"
+    install_application_from_url "$google_drive_url"
     install_application_from_url "$air_server_url"
     install_application_from_url "$crisis_go"
     install_application_from_url "$cannon_driver"
+    install_application_from_url "$visualizer_url"  
     install_application_from_url "$onyx_url"
 }
 
@@ -85,28 +152,69 @@ install_applications_office(){
     install_application_from_url "$app_cleaner_url"
     install_application_from_url "$zoom_url"
     install_application_from_url "$google_chrome_url"
+    install_application_from_url "$google_drive_url"
     install_application_from_url "$crisis_go"
     install_application_from_url "$cannon_driver"
+    install_application_from_url "$onyx_url"
+
+}
+
+install_applications_everything(){
+    add_everyone_to_printer_group
+    #slow 
+    install_application_from_url "$word_url"
+    install_application_from_url "$excel_url"
+    install_application_from_url "$powerpoint_url"
+    #Normal 
+    install_application_from_url "$app_cleaner_url"
+    install_application_from_url "$zoom_url"
+    fullcleanupSmartSoftware
+    install_application_from_url "$smart_notebook_url"
+    start_smart_software
+    install_application_from_url "$google_chrome_url"
+    install_application_from_url "$air_server_url"
+    install_application_from_url "$crisis_go"
+    install_application_from_url "$cannon_driver"
+    install_application_from_url "$visualizer_url"  
+    install_application_from_url "$onyx_url"
+
 }
 
 list_and_install_applications() {
-    echo "Please select the applications you would like to install by entering the corresponding numbers separated by spaces:"
+    echo -e "${NC}Please select the applications you would like to install by entering the corresponding numbers separated by spaces and pressing enter:${NC}"
+    echo -e "${NC}(Type 'back' to go back to the main menu)${NC}"
+    
     options=(
-        "App Cleaner"
-        "Zoom"
-        "Smart Notebook"
-        "Google Chrome"
-        "Air Server"
-        "Crisis Go"
-        "Canon Driver"
-        "OnyX"
+        "${BROWN}App Cleaner${NC}"
+        "${BLUE}Zoom${NC}"
+        "${CYAN}Smart Notebook${NC}"
+        "${YELLOW}Google Chrome${NC}"
+        "${MAGENTA}Google Drive${NC}"
+        "${NC}Air Server${NC}"
+        "${RED}Crisis Go${NC}"
+        "${NC}Canon Driver${NC}"
+        "${BLUE}Visualizer${NC}"
+        "${MAGENTA}OnyX${NC}"
+        "${BLUE}Microsoft Word${NC}"
+        "${GREEN}Microsoft Excel${NC}"
+        "${ORANGE}Microsoft PowerPoint${NC}"
     )
 
     for i in "${!options[@]}"; do
-        echo "$((i+1)). ${options[i]}"
+        echo -e "$((i+1)). ${options[i]}"
     done
 
     read -p "Enter your choices: " -a choices
+
+    # Check if user wants to go back
+    if [ "${choices[0]}" = "back" ]; then
+        clear
+        first_choice
+        return
+    fi
+
+    # Add everyone to printer group after selection but before installation
+    add_everyone_to_printer_group
 
     for choice in "${choices[@]}"; do
         case $choice in
@@ -114,30 +222,127 @@ list_and_install_applications() {
             2) install_application_from_url "$zoom_url" ;;
             3) 
                 # If Smart Notebook is selected, remove the SMART Technologies folder
-                cleanupSmartSoftware
+                fullcleanupSmartSoftware
                 install_application_from_url "$smart_notebook_url"
+                start_smart_software
                 ;;
             4) install_application_from_url "$google_chrome_url" ;;
-            5) install_application_from_url "$air_server_url" ;;
-            6) install_application_from_url "$crisis_go" ;;
-            7) install_application_from_url "$cannon_driver" ;;
-            8) install_application_from_url "$onyx_url" ;;
+            5) install_application_from_url "$google_drive_url" ;;
+            6) install_application_from_url "$air_server_url" ;;
+            7) install_application_from_url "$crisis_go" ;;
+            8) install_application_from_url "$cannon_driver" ;;
+            9) install_application_from_url "$visualizer_url" ;;
+            10) install_application_from_url "$onyx_url" ;;
+            11) install_application_from_url "$word_url" ;;
+            12) install_application_from_url "$excel_url" ;;
+            13) install_application_from_url "$powerpoint_url" ;;
             *) echo "Invalid choice: $choice" ;;
         esac
     done
 }
 
+# first_choice(){
+#     read -p "Will This computer be used with a Smart Board? (y/n) or would you like to manually select applications? (m)" answer
+#     if [[ "$answer" == "y" ]]; then
+#         #remove smart technologies folder from /Applications to prevent errors
+#         fullcleanupSmartSoftware
+#         install_applications_teacher
+#     elif [[ "$answer" == "n" ]]; then
+#         install_applications_office
+#     elif [[ "$answer" == "m" ]]; then
+#         list_and_install_applications
+#     else
+#         echo "Invalid choice: $answer"
+#     fi
+# }
+
 first_choice(){
-    read -p "Will This computer be used with a Smart Board? (y/n) or would you like to manually select applications? (m)" answer
-    if [[ "$answer" == "y" ]]; then
-        #remove smart technologies folder from /Applications to prevent errors
-        cleanupSmartSoftware
-        install_applications_teacher
-    elif [[ "$answer" == "n" ]]; then
-        install_applications_office
-    elif [[ "$answer" == "m" ]]; then
-        list_and_install_applications
-    else
-        echo "Invalid choice: $answer"
-    fi
+    echo "Please select an option by entering the corresponding number and pressing enter:"
+    echo -e "${YELLOW}1. Update using Teacher preset${NC}"
+    echo -e "${CYAN}2. Update using Office Macmini preset${NC}"
+    echo -e "${GREEN}3. Update everything - This will take a while${NC}" 
+    echo -e "${MAGENTA}4. Manually select what to update${NC}"
+    echo -e "${RED}5. More Options${NC}"
+    echo -e "${BLUE}6. Exit${NC}"
+    
+    read -p "Enter your choice (1-6): " choice
+    
+    case $choice in
+        1)
+            clear
+            install_applications_teacher
+            ;;
+        2)
+            clear
+            install_applications_office
+            ;;
+        3)
+            clear
+            install_applications_everything
+            ;;
+        4)
+            clear
+            list_and_install_applications
+            ;;
+        5)
+            clear
+            more_options
+            ;;
+        6)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            clear
+            echo "Invalid choice: $choice"
+            first_choice
+            ;;
+    esac
+}
+
+more_options(){
+    echo "Please select an option by entering the corresponding number and pressing enter:"
+    echo -e "${YELLOW}1. Add everyone to the printer group${NC}"
+    echo -e "${CYAN}2. Stop SMART software${NC}"
+    echo -e "${GREEN}3. Start SMART software${NC}" 
+    echo -e "${BLUE}4. Restart SMART software${NC}"
+    echo -e "${MAGENTA}5. Main Menu${NC}"
+    echo -e "${RED}6. Exit${NC}"
+    
+    read -p "Enter your choice (1-6): " choice
+    
+    case $choice in
+        1)
+            clear
+            add_everyone_to_printer_group
+            more_options
+            ;;
+        2)
+            clear
+            stop_smart_software
+            more_options
+            ;;
+        3)
+            clear
+            start_smart_software
+            more_options
+            ;;
+        4)
+            clear
+            first_choice
+            ;;
+        5)  
+            clear
+            restart_smart_software
+            more_options
+            ;;
+        6)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice: $choice"
+            first_choice
+            ;;
+    esac
 }
